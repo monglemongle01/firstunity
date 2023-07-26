416a59cf534f980163062226cef4fbfb516eed27
sfc7 <140380337+monglemongle01@users.noreply.github.com>
2023-07-26 17:39:14 +0900

    (2023.7.26) 스크립트 생산

---
+++ unity Tutorial/Assets/Script/Car.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Car : MonoBehaviour
+{   // 클래스 시작 위치
+    
+    // 위에서 부터 아래로 프로그램이 실행됩니다.
+
+
+}   // 클래스가 끝나는 위치
