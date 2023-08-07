6b9121590120911652e3d55c579e7b5c29059173
monglemongle01 <rlaehddms233@gmail.com>
2023-08-07 17:45:11 +0900

    (2023.8.7) Animation 제작

---
+++ unity Tutorial/Assets/Unity Animator Controllor/Script/ScriptManager.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScriptAnimantion : MonoBehaviour
+{
+    private Animator animator = null;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            animator.SetTrigger("Fast Run");
+        }
+    }
+}
